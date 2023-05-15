71b48ab13652221f89d9ea1831aaec95c28c29bc
Drii <102504203+Driirhmnn@users.noreply.github.com>
2023-05-15 23:52:55 +0800

    Add files via upload

---
+++ Modul_5/Peminjaman.php
@@ -0,0 +1,53 @@
+<?php 
+require('model.php');
+if (isset($_GET['id_peminjaman'])){
+    hapuspeminjaman($_GET['id_peminjaman']);
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data Peminjaman </title>
+    <style>
+        body { 
+            width: 900px;
+            margin:auto;}
+        table{
+            border-collapse: collapse;
+        }
+        tr{
+            border: black;
+        }
+        th{
+            background-color: #a0a0a0;
+        }
+        td{
+            background-color: #E8E8E8;
+        }
+    </style>
+</head>
+<body>
+    <h2 style = "text-align:center">Peminjaman</h2>
+    <table border = "1">
+        <thead>
+            <tr>
+                <th>ID Peminjaman</th>
+                <th>Tanggal Peminjaman</th>
+                <th>Tanggal Kembali</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            tampilpeminjaman();
+            ?>
+        </tbody>
+    </table>
+    <br></br>
+    <a href="formpeminjaman.php"><button>Tambah Data Peminjaman</button></a> <br></br>
+    <a href="index.php"><img src="gambar/house.png" alt="" height="50"width="50"></a>
+    <a href="Buku.php"><img src="gambar/book.png" alt="" height = "70" width = "100"></a>
+    <a href="Member.php"><img src="gambar/team.png" alt="" height ="70"width = "75"></a>
+    </div>
+</body>
+</html>
